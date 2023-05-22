4274613f1ce1bc6bee8df9e5c133697c5cb04ee9
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-05-22 08:30:31 +0200

    Delete spanc_20_vues_gestion.sql

--- bdd/spanc_20_vues_gestion.sql
+++
@@ -1,7 +0,0 @@
-/*SPANC V1.0*/
-/*Creation du squelette de la structure des données (view) */
-/* spanc_20_vues_gestion.sql */
-/*PostGIS*/
-
-/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
-/* Auteur : Grégory Bodet */
