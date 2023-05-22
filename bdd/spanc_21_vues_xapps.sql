684659129d3cf9c81c180d46e3d4da21a3d747f3
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-05-22 08:31:22 +0200

    Create spanc_21_vues_xapps.sql

---
+++ bdd/spanc_21_vues_xapps.sql
@@ -0,0 +1,7 @@
+/*SPANC V1.0*/
+/*Creation du squelette de la structure des données (view) */
+/* spanc_21_vues_xapps.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
