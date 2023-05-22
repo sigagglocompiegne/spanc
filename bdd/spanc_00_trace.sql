7dbf1f00f4952141ca48cff7da33b5d0fcce30a2
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-05-22 08:26:33 +0200

    Create spanc_00_trace.sql

---
+++ bdd/spanc_00_trace.sql
@@ -0,0 +1,12 @@
+/*spanc V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* spanc_10_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+
+-- 2023/05/22 : GB / Initialisation du code SQL pour la création de structure de la base de données
