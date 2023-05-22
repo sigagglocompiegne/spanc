d86fc56df0194efb0caad3e507a787d801e1d62a
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-05-22 08:29:56 +0200

    Create spanc_20_vues_gestion.sql

---
+++ bdd/spanc_20_vues_gestion.sql
@@ -0,0 +1,7 @@
+/*SPANC V1.0*/
+/*Creation du squelette de la structure des données (view) */
+/* spanc_20_vues_gestion.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
