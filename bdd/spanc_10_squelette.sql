bf6a826028901b543075934ae916d9effe12baaa
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-08-30 11:00:26 +0200

    Update spanc_10_squelette.sql

--- bdd/spanc_10_squelette.sql
+++ bdd/spanc_10_squelette.sql
@@ -446,6 +446,8 @@ INSERT INTO m_spanc.lt_spanc_patro(
     ('11','Monsieur'),
     ('12','Madame'),
     ('20','Madame & Monsieur'),
+    ('21','Monsieur & Monsieur'),
+    ('22','Madame & Madame'),  	
     ('30','Mesdames & Messieurs'),
     ('40','SCI'),
     ('99','Autre');
