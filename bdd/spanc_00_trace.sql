80faa1aab415e0bebb987845269d493f303ba905
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-10-31 09:51:39 +0100

    Update spanc_00_trace.sql

--- bdd/spanc_00_trace.sql
+++ bdd/spanc_00_trace.sql
@@ -9,6 +9,7 @@
 /*
 #################################################################### SUIVI CODE SQL ####################################################################
 
+-- 2023/10/31 : GB / Ajustement de la fonction générant les clés primaires de contrôles
 -- 2023/08/30 : GB / Ajout de patronymes
 -- 2023/08/27 : GB / Ajustement de structures et des vues fonctionnelles suite intégration d'un jeu de données
 -- 2023/08/07 : GB / Ajustement suite traitement des insertions des contrôles CCLO
